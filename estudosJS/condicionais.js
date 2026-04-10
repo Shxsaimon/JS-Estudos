211910000300c1c45f3b122ccea8e3dafd569e3c
Saimon <saimonblogoslawski@gmail.com>
2026-04-09 22:00:43 -0300

    Mais de condicionais, usando condições aninhadas e switch. Cálculo de datas está incorreto pois a verificação do dia trás um dia na frente como o correto, à corrigir

--- estudosJS/condicionais.js
+++ estudosJS/condicionais.js
@@ -4,11 +4,40 @@ function verificarVelocidade() {
     var velocidade = Number(document.querySelector("#velocidade").value);
     
     if (velocidade > limiteVelocidade) {
-        alert(`Você ultrapassou o limite de velocidade! Você será multado. Velocidade registrada: ${velocidade} km/h`);
+        document.getElementById("mensagemvelocidade").innerText = `Você ultrapassou o limite de velocidade e será multado.`;
     } else {
-        alert("Você está dentro do limite de velocidade.");
+        document.getElementById("mensagemvelocidade").innerText = "Você está dentro do limite de velocidade.";
     }
 }
-
 // Atualiza o texto do limite no HTML ao carregar a página
 document.getElementById("limitevelocidade").innerText = `Limite de velocidade: ${limiteVelocidade} km/h`;
+
+var idademinimavoto = 16; // Idade mínima para votar
+var idademaximavoto = 65; // Idade máxima para voto obrigatório
+function verificarIdade() {
+    var idade = Number(document.querySelector("#idade").value);
+    if (idade < idademinimavoto) {
+        document.getElementById("mensagemidade").innerText = "Você não tem idade para votar.";
+    } else if (idade >= idademinimavoto && idade < 18 || idade >= idademaximavoto) {
+        document.getElementById("mensagemidade").innerText = "O voto é opcional para você.";
+    } else {
+        document.getElementById("mensagemidade").innerText = "O voto é obrigatório para você.";
+    }
+}
+document.getElementById("idadevoto").innerText = `Idade mínima para votar: ${idademinimavoto} anos`;
+
+function verificarData() {
+    var data = new Date(document.querySelector("#data").value);
+    var hoje = new Date();
+    var diffDays = Math.floor((hoje - data) / (1000 * 60 * 60 * 24));
+    switch (true) {
+        case diffDays < 0:
+            document.getElementById("mensagemdiasemana").innerText = "A data selecionada é no futuro.";
+            break;
+        case diffDays === 0:
+            document.getElementById("mensagemdiasemana").innerText = "A data selecionada é hoje.";
+            break;
+        default:
+            document.getElementById("mensagemdiasemana").innerText = `A data selecionada foi há ${diffDays} dias.`;
+    }
+}
